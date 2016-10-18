b2bb7e6038a1ad485d2f0a3114d9e12c2fa5f730
Dave Bosley <dave.l.bosley@gmail.com>
2016-10-18 08:15:47 -0600

    auto-generating API routes working

--- keystone.js
+++ keystone.js
@@ -1,89 +1,96 @@
 require('dotenv').config();
 
 var keystone = require('keystone'),
-	webpack = require('webpack'),
-	MemoryFS = require("memory-fs"),
-	path = require('path'),
-	devMiddleware = require('webpack-dev-middleware'),
-	hotMiddleware = require('webpack-hot-middleware'),
-	config = require('./webpack.config.js'),
-	compiler = webpack(config);
+    keystoneRestApi = require('keystone-rest-api'),
+    webpack = require('webpack'),
+    MemoryFS = require("memory-fs"),
+    path = require('path'),
+    devMiddleware = require('webpack-dev-middleware'),
+    hotMiddleware = require('webpack-hot-middleware'),
+    config = require('./webpack.config.js'),
+    compiler = webpack(config);
 
-var fs = new MemoryFS();
-compiler.outputFileSystem = fs;
+var mem_fs = new MemoryFS();
+compiler.outputFileSystem = mem_fs;
 
 console.log("Environment:" + process.env.NODE_ENV);
-var isDev = process.env.NODE_ENV == 'development' ;
+var isDev = process.env.NODE_ENV == 'development';
 
 keystone.init({
-	'name': 'keystone-angular2-webpack-starter',
-	'brand': 'keystone-angular2-webpack-starter',
+    'name': 'keystone-angular2-webpack-starter',
+    'brand': 'keystone-angular2-webpack-starter',
 
-	'static': 'src/public',
-	'favicon': 'src/public/favicon.ico',
+    'static': 'src/public',
+    'favicon': 'src/public/favicon.ico',
 
-	'emails': 'templates/emails',
+    'emails': 'templates/emails',
 
-	'auto update': true,
-	'session': true,
-	'auth': true,
-	'user model': 'User',
-	'cookie secret': '(my secret)'
+    'auto update': true,
+    'session': true,
+    'auth': true,
+    'user model': 'User',
+    'cookie secret': '(my secret)'
 });
+
 // Load your project's Models
 keystone.import('models');
 
-keystone.set('routes', function(app) {
-	if (isDev) {
-		app.use(devMiddleware(compiler, {
-			publicPath: config.output.publicPath
-		}));
-	
-		app.use(hotMiddleware(compiler, {
-			log: console.log
-		}));
-	}
+keystone.set('routes', function (app) {
+    if (isDev) {
+        app.use(devMiddleware(compiler, {
+            publicPath: config.output.publicPath
+        }));
 
-	app.all('*', function (req, res, next) {
-		var filename = path.join(compiler.outputPath, 'index.html');
-		console.log('file:' + filename);
-		compiler.outputFileSystem.readFile(filename, function(err, result) {
-			if (err) {
-				return next(err);
-			}
-			res.set('content-type', 'text/html');
-			res.send(result);
-			res.end();
-		});
-	});
+        app.use(hotMiddleware(compiler, {
+            log: console.log
+        }));
+    }
+
+    // Add routes with Keystone
+    keystoneRestApi.createRest(keystone, {
+        apiRoot: '/api/v1/'
+    });
+
+    app.get('*', function (req, res, next) {
+        var filename = path.join(compiler.outputPath, 'index.html');
+        console.log('file:' + filename);
+        compiler.outputFileSystem.readFile(filename, function (err, result) {
+            if (err) {
+                return next(err);
+            }
+            res.set('content-type', 'text/html');
+            res.send(result);
+            res.end();
+        });
+    });
 });
 
 // Setup common locals for your templates. The following are required for the
 // bundled templates and layouts. Any runtime locals (that should be set uniquely
 // for each request) should be added to ./routes/middleware.js
 keystone.set('locals', {
-	_: require('lodash'),
-	env: keystone.get('env'),
-	utils: keystone.utils,
-	editable: keystone.content.editable,
+    _: require('lodash'),
+    env: keystone.get('env'),
+    utils: keystone.utils,
+    editable: keystone.content.editable,
 });
 
 
 // Setup common locals for your emails. The following are required by Keystone's
 // default email templates, you may remove them if you're using your own.
 keystone.set('email locals', {
-	logo_src: '/img/angular.png',
-	logo_width: 194,
-	logo_height: 76,
-	theme: {
-		email_bg: '#f9f9f9',
-		link_color: '#2697de',
-		buttons: {
-			color: '#fff',
-			background_color: '#2697de',
-			border_color: '#1a7cb7',
-		},
-	},
+    logo_src: '/img/angular.png',
+    logo_width: 194,
+    logo_height: 76,
+    theme: {
+        email_bg: '#f9f9f9',
+        link_color: '#2697de',
+        buttons: {
+            color: '#fff',
+            background_color: '#2697de',
+            border_color: '#1a7cb7',
+        },
+    },
 });
 
 // Load your project's email test routes
@@ -91,7 +98,7 @@ keystone.set('email locals', {
 
 // Configure the navigation bar in Keystone's Admin UI
 keystone.set('nav', {
-	users: 'users',
+    users: 'users',
 });
 
 // Start Keystone to connect to your database and initialise the web server
--- models/User.js
+++ models/User.js
@@ -5,27 +5,40 @@ var Types = keystone.Field.Types;
  * User Model
  * ==========
  */
-var User = new keystone.List('User');
+var User = new keystone.List('User', {
+    rest: true,
+    restOptions: 'list show create update delete'
+});
 
 User.add({
-	name: { type: Types.Name, required: true, index: true },
-	email: { type: Types.Email, initial: true, required: true, index: true },
-	password: { type: Types.Password, initial: true, required: true },
+    name: {
+        type: Types.Name,
+        required: true,
+        index: true
+    },
+    email: {
+        type: Types.Email,
+        initial: true,
+        required: true,
+        index: true
+    },
+    password: {
+        type: Types.Password,
+        initial: true,
+        required: true
+    },
 }, 'Permissions', {
-	isAdmin: { type: Boolean, label: 'Can access Keystone', index: true },
+    isAdmin: {
+        type: Boolean,
+        label: 'Can access Keystone',
+        index: true
+    },
 });
 
 // Provide access to Keystone
 User.schema.virtual('canAccessKeystone').get(function () {
-	return this.isAdmin;
+    return this.isAdmin;
 });
-
-
-/**
- * Relationships
- */
-User.relationship({ ref: 'Post', path: 'posts', refPath: 'author' });
-
 
 /**
  * Registration
